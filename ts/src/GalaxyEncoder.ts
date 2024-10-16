242177739bf029432df3a77f53d26f0c10bea9b8
antonio Garibay <GaribayJAMZXN@outlook.com>
2024-10-16 10:28:03 -0600

    new

--- ts/src/GalaxyEncoder.ts
+++ ts/src/GalaxyEncoder.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function GalaxyHexEncoder(textToEncode: string, lenKey: number): string {
     const encoder = new TextEncoder();  
     const encoded = encoder.encode(textToEncode);
@@ -32,6 +33,26 @@ export function GalaxyDecoder(encodedBytes: number[], lenKey: number): number[] 
         decodedText.push(binaryChanger(clavehex, results[0][i], true));
     }
     return decodedText;
+=======
+export function GalaxyEncoder(text: string, bytesToUse: number): string {
+    const clave = Array.from({ length: bytesToUse }, () => Math.floor(Math.random() * 256));
+    const results = buildText(text.split('').map((char) => {
+        return binaryChanger(clave, char.charCodeAt(0), false).toString(16).padStart(2, '0'); // Convierte a hexadecimal
+    }).join(''), clave, bytesToUse,false);
+    return results[0];
+}
+export function GalaxyDecoder(textencode: string, bytesUsed: number): string {
+    const results = buildText(textencode, [], bytesUsed, true);
+    let clavehex: number[] = [];
+    for (let i = 0; i < results[1].length; i += 2) {
+        clavehex.push(parseInt(results[1].slice(i, i + 2), 16));
+    }
+    var decodedTextInHex: string = "";
+    for (let i = 0; i < results[0].length; i += 2) {
+        decodedTextInHex += String.fromCharCode(binaryChanger(clavehex, parseInt(results[0].slice(i, i + 2), 16), true));
+    }
+    return decodedTextInHex;
+>>>>>>> 9607959 (cambiando la configuración para typescript y npm)
 }
 function binaryChanger(clave: number[],char: number, decode: boolean): number{
     var sortedClave = [...clave].sort((a, b) => b - a);
@@ -52,6 +73,7 @@ function binaryChanger(clave: number[],char: number, decode: boolean): number{
     }
     return parseInt(charBinary, 2);
 }
+<<<<<<< HEAD
 function buildText(text: number[], clave: number[], bytesToUse: number,decode: boolean): [number[], number[]]{
     switch (true) {
         case bytesToUse <= 4 && decode:
@@ -69,3 +91,22 @@ function buildText(text: number[], clave: number[], bytesToUse: number,decode: b
     }
     return [[], []];
 }
+=======
+function buildText(text: string, clave: number[], bytesToUse: number,decode: boolean): [string, string]{
+    switch (true) {
+        case bytesToUse <= 4 && decode:
+            return [text.slice(0, -(bytesToUse * 2)), text.slice(-(bytesToUse * 2))];
+        case bytesToUse <= 4:
+            return [text + Array.from(clave, (byte) => byte.toString(16).padStart(2, '0')).join(''), ""];
+        case bytesToUse <= 7 && decode:
+            return [text.slice((bytesToUse*2-8), -8), text.slice(-8) + text.slice(0, (bytesToUse*2-8))]
+        case bytesToUse <= 7:
+            return [Array.from(clave.slice(4), (byte) => byte.toString(16).padStart(2, '0')).join('') + text + Array.from(clave.slice(0, 4), (byte) => byte.toString(16).padStart(2, '0')).join(''), ""];
+        case bytesToUse <= 10 && decode:
+            return [text.slice(6, -(bytesToUse*2-6)), text.slice(-8) + text.slice(0, 6) + text.slice(-(bytesToUse*2-6), -8)];
+        case bytesToUse <= 10:
+            return [Array.from(clave.slice(4, 7), (byte) => byte.toString(16).padStart(2, '0')).join('') + text + Array.from(clave.slice(7), (byte) => byte.toString(16).padStart(2, '0')).join('')  + Array.from(clave.slice(0, 4), (byte) => byte.toString(16).padStart(2, '0')).join(''), ""];
+    }
+    return ["", ""];
+}
+>>>>>>> 9607959 (cambiando la configuración para typescript y npm)
